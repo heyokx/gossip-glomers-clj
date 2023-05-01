1c2db7e2773110dd3897a6e0b648bce0fe60ca31
Charles-William Desroches <charles-william.desroches@busbud.com>
2023-05-01 08:04:18 -0400

    setup: clj development setup with repl + portal

---
+++ challenges/echo-challenge/dev/core.clj
@@ -0,0 +1,22 @@
+(ns dev.core
+  (:require [portal.api :as p]
+            [heyokx.echo-challenge :as main]))
+
+(defn start-portal-vs-code []
+  (p/open {:launcher :vs-code}))
+
+(tap> (main/echo))
+
+(comment
+  
+  (def portal-session (start-portal-vs-code)) ; create portal inspector session
+  (add-tap #'p/submit) ; add portal as a tap> target
+
+  (tap> "test!")
+
+  (p/clear)
+
+  (p/close)
+
+  )
+
--- challenges/echo-challenge/src/heyokx/echo_challenge.clj
+++ challenges/echo-challenge/src/heyokx/echo_challenge.clj
@@ -1,10 +1,22 @@
 (ns heyokx.echo-challenge
-  (:gen-class))
+  (:gen-class)
+  (:require [cheshire.core :as c]))
 
 (defn greet
   "Callable entry point to the application."
   [data]
   (println (str "Hello, " (or (:name data) "World") "!")))
+
+(defn echo
+  "Echo function!"
+  []
+  (let [content (c/generate-string {:src "a string identifying the node this message came from" 
+                                    :dest "a string identifying the node this message is to"
+                                    :body {
+                                           :a 12
+                                           :b "Blah"
+                                           }})]
+    content))
 
 (defn -main
   "I don't do a whole lot ... yet."
